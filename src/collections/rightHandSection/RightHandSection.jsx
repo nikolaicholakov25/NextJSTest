a381310dfa80baa3184c0b768b275e07b79463be
Nikolai_Cholakov <nikolaicholakov25@gmail.com>
2022-09-05 10:48:41 +0300

    added icons and finished the desctop version

--- src/collections/rightHandSection/RightHandSection.jsx
+++ src/collections/rightHandSection/RightHandSection.jsx
@@ -4,12 +4,16 @@ export const RightHandSection = () => {
 
     const text1 = <p>Complete <span>brief writting or simple guidance</span> on what to include, we got you covered.</p>
 
+    const text2 = <p>In-Depth agency search covering; <span>criteria matching,</span> door knocking and due-diligence vetting.</p>
+
+    const text3 = <p>Comprehensive <span>pitch management</span>, including comms, diary, managment and pitch hosting.</p>
+
 
     return (
         <div className="rightSection">
-                <TextBox id='box1' title='Brief' text={text1}/>
-                <TextBox id='box2' title='Brief' text={text1}/>
-                <TextBox id='box3' title='Brief' text={text1}/>
+                <TextBox id='box1' icon='fa-solid fa-clipboard' title='Brief' text={text1}/>
+                <TextBox id='box2' icon='fa-solid fa-magnifying-glass' title='Search' text={text2}/>
+                <TextBox id='box3' icon='fa-solid fa-people-group' title='Pitch' text={text3}/>
         </div>
     )
 }
--- src/components/textBox/TextBox.jsx
+++ src/components/textBox/TextBox.jsx
@@ -1,7 +1,8 @@
-export const TextBox = ({title,text,id}) => {
+export const TextBox = ({title,text,id,icon}) => {
+    console.log(icon);
     return (
         <div className="textBox" id={id}>
-            <div className="imgBox"></div>
+            <div className="imgBox"><i className={icon}></i></div>
             <div className="textArea">
                 <h2>{title}</h2>
                 <p>{text}</p>
